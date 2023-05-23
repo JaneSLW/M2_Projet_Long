8ec2a51469f9b42e8007186c4d9722be9bf06e87
JaneSLW <jane.law1999@hotmail.com>
2023-05-23 23:00:49 +0200

    Comments updated

--- src/main.sh
+++ src/main.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#$1 reference voir 
-#$2 repertoire de travail 
-#$3 fichier bed de départ
-REFNAME=${1}
-WDIR=${2}
-BEDold=${3}
-STARTS=${4} # ex  ( 1 3 )
-STOPS=${5} # ex (30 31)
+REFNAME=${1}	#$1 reference
+WDIR=${2}	# working directory
+BEDold=${3}	# bed file
+STARTS=${4} # example  (1 3)
+STOPS=${5}	# example (20 21)
 
 echo $REFNAME $WDIR $BEDold
 FNAME=`basename ${REFNAME} | cut -d"." -f1`
@@ -17,17 +14,17 @@ BED=`basename ${BEDold}`
 cp ${BEDold} ${WDIR}/${BED}
 
 cd ${WDIR}
-# dos2unix ${REFNAME}.bed
 
-# divise le fichier bed en fonction de la longuer de l'exon
-awk '{if ( ($3-$2+1) <=25) {printf("%s\n",$0);}}' ${BED} > BED_600.bed # si l'exon a moins de 25 bases alors on attend au moin 60% de match
-awk '{if ((($3-$2+1) <=50) && (($3-$2+1)>25)) {printf("%s\n",$0);}}' ${BED} > BED_185.bed # 18,5%
+# Dividing bed file based on the length of the exon
+# Example : If the exon is less than 25 bp then the match threshold is 60 %
+awk '{if ( ($3-$2+1) <=25) {printf("%s\n",$0);}}' ${BED} > BED_600.bed 
+awk '{if ((($3-$2+1) <=50) && (($3-$2+1)>25)) {printf("%s\n",$0);}}' ${BED} > BED_185.bed
 awk '{if ((($3-$2+1) <=80) && (($3-$2+1)>50)) {printf("%s\n",$0);}}' ${BED} > BED_140.bed
 awk '{if ((($3-$2+1) <=100) && (($3-$2+1)>80)) {printf("%s\n",$0);}}' ${BED} > BED_125.bed
 awk '{if (($3-$2+1) >100) {printf("%s\n",$0);}}' ${BED} > BED_100.bed
 
 for L in 600 185 140 125 100 ; do
-bedtools intersect -f 0.${L} -wo -a BED_${L}.bed -b ../${REFNAME}  # filtre les reads avec le nb de match minimum
+bedtools intersect -f 0.${L} -wo -a BED_${L}.bed -b ../${REFNAME}
 done > ${FNAME}.txt
 sort -k8,8 -k2,2n ${FNAME}.txt > ${FNAME}_sorted.txt
 
@@ -36,7 +33,7 @@ ALL_Exons=`awk '{if (NR==1) printf("%s",$4); else printf(",%s",$4);}END{printf("
 cut -f4,8,10 ${FNAME}_sorted.txt | uniq | awk -v allexons=${ALL_Exons} 'BEGIN{n=split(allexons,a,","); }{  if ($2 != old){if (NR>1) {while (i<=n){printf("0"); i=i+1;} printf("\t%s\t",old);} i=1; for (k=1;k<j; k++){printf("%s",b[k]);} printf("\n"); old=$2;j=1;} while (i<=n){if (a[i]!=$1){printf("0");i=i+1;}else{ i=i+1; printf("1"); break;}  } b[j]=$3; j=j+1;}' | sort -k1,1nr > ${FNAME}_vector.txt
  
 cat ${FNAME}_vector.txt | grep -v -E "\+\-|\-\+" | awk -v allexons=${ALL_Exons} 'BEGIN{n=split(allexons,a,","); }{nn=split($1,b,"");for(i=1; i<=n; i++){ if (b[i]=="1"){ if (a[i] ~ /ASS5/){ if (b[i-1]=="1"){printf("1");}else{printf("0");} }else{ if (a[i] ~ /ASS3/){ if (b[i+1]=="1"){printf("1"); }else{printf("0");} }else{ printf("1"); } } }else{printf("0");} } printf("\t%s\t%s\n",$2,$3); }' > ${FNAME}_vector_corrected.txt
-# ~ = match 
+
 cat ${FNAME}_vector_corrected.txt | cut -f1 | sort | uniq -c | sort -k1,1nr| awk '{printf("%s\t%s\n",$1,$2);}' > ${FNAME}_abundance_filter.txt 
 
 for i in "${STARTS[@]}"
@@ -44,7 +41,7 @@ do
 	# Start pattern
 	NAME="Exon_${i}$"
 	IND=`grep -n ${NAME} ${BED} | cut -d : -f1`
-	pat_s=`pat_s=$(printf "%${IND}s");echo ${pat_s// /.}` #"..."
+	pat_s=`pat_s=$(printf "%${IND}s");echo ${pat_s// /.}`
 	
 	if [ ${i} -gt 1 ];
 	then
